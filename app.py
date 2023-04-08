b6425f43d2c2b4a23f48cf310482ee4efce2cc21
root-master <yrafati@gmail.com>
2023-04-07 21:46:43 -0400

    removed cors

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask, jsonify
 from flask import request
 from model import recommend_workout
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
 
 
 @app.route("/", methods=["GET"])
