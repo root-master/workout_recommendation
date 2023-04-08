f394164c5b6553721e87c550f054cdf54d43e323
root-master <yrafati@gmail.com>
2023-04-08 11:12:28 -0400

    added CORS

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify
 from flask import request
+from flask_cors import CORS
 from model import recommend_workout
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/")
