22bb5479d5a98bd75976222c34ad8f31aaf95fe4
root-master <yrafati@gmail.com>
2023-04-07 21:43:54 -0400

    renamed app

--- app.py
+++ app.py
@@ -3,16 +3,16 @@ from flask import request
 from model import recommend_workout
 from flask_cors import CORS
 
-flask_app = Flask(__name__)
-CORS(flask_app)
+app = Flask(__name__)
+CORS(app)
 
 
-@flask_app.route("/", methods=["GET"])
+@app.route("/", methods=["GET"])
 def get():
     return "Flask app is working!"
 
 
-@flask_app.route("/recommend_workout", methods=["POST", "GET"], strict_slashes=False)
+@app.route("/recommend_workout", methods=["POST", "GET"], strict_slashes=False)
 def recommend():
     if request.method == "POST":
         try:
@@ -29,4 +29,4 @@ def recommend():
 
 
 if __name__ == "__main__":
-    flask_app.run()
+    app.run()
